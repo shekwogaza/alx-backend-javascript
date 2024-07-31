9e02cdf52f8ebaf8a7ba69bd1739f07ab8eccf91
GEEGII <gazasolomon@gmail.com>
2024-07-31 01:27:24 +0100

    update to clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,13 @@
 export default function cleanSet(set, startString) {
-  if (!startString || startString.length === 0 || !set || set.length <= 0) {
+  if (!startString || startString.length === 0 || !(set instanceof Set) || set.size === 0) {
     return '';
   }
+
   return [...set]
-    .filter((value) => value.startsWith(startString))
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
     .map((value) => value.slice(startString.length))
     .join('-');
+
   // let result = '';
   // for (const value of set) {
   //     if (value.startsWith(startString)) {
