86b0a52dafdb44820624a11343b03fb5a4642779
GEEGII <gazasolomon@gmail.com>
2024-08-01 01:17:27 +0100

    lint compliant

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,17 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
+    uploadPhoto(fileName),
   ];
 
   return Promise.allSettled(promises)
-    .then(results => results.map(result => ({
+    .then((results) => results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled'
         ? result.value
-        : String(result.reason)
+        : String(result.reason),
     })));
 }
